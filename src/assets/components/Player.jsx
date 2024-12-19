d513e03c9f6d17fbcfdfa6aa0ef6b971371a04ba
matheusingani <matheusingani@hotmail.com>
2024-12-19 14:24:36 +1100

    tic-tac

---
+++ src/assets/components/Player.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick(e) {
+    setIsEditing((prevState) => !prevState); // Don't do that (!isEditing) to update
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handlerPlayerName(e) {
+    setPlayerName(e.target.value);
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {isEditing ? (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handlerPlayerName}
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
